7a43e95d11b9f9937b60da07c90949b69a280931
Yiliang Chen <yiliangchen0113@gmail.com>
2020-09-23 21:13:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -109,11 +109,9 @@ end
 
 
 def play(board)
-  round = 0
-  while round < 9 && !won?(board) && !draw?(board)
-  turn(board)
-  round += 1
-  end
+
+
+
   if won?(board)
     puts "Congratulations #{winner(board)}!"
   end
