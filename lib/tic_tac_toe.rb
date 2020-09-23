3789cfb9f97c7df1a5093b3b3a4c22d3d15e070b
Yiliang Chen <yiliangchen0113@gmail.com>
2020-09-23 21:04:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,14 @@
+WIN_COMBINATIONS = [
+  [0,1,2], 
+  [3,4,5], 
+  [6,7,8], 
+  [0,3,6],
+  [1,4,7], 
+  [2,5,8], 
+  [0,4,8], 
+  [2,4,6] 
+]
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -10,16 +21,48 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-WIN_COMBINATIONS = [
-  [0,1,2], 
-  [3,4,5], 
-  [6,7,8], 
-  [0,3,6],
-  [1,4,7], 
-  [2,5,8], 
-  [0,4,8], 
-  [2,4,6] 
-]
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    player = current_player(board)
+    move(board, index, player)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  turn = 0
+  board.each do |i| 
+  if i != " "
+  turn += 1
+  end
+  end
+  return turn
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 1 ? player = "O" : player = "X"
+end
 
 def won?(board)
  WIN_COMBINATIONS.each do |win_combination|
@@ -65,52 +108,8 @@ def winner(board)
 end 
 
 
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
-
-def move(board, index, current_player)
-  board[index] = current_player
-end
-
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
-end
-
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
-
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    player = current_player(board)
-    move(board, index, player)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
-
-
-def turn_count(board)
-  turn = 0
-  board.each do |i| 
-if i != " "
-  turn += 1
-  end
-  end
-  return turn
-end
-
-def current_player(board)
-  turn_count(board) % 2 == 1 ? player = "O" : player = "X"
-end
-
 def play(board)
-  round = 0 
+  round = 02
   while round < 9 
   turn(board)
   round += 1 
